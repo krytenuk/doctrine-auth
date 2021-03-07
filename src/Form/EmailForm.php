348508abc27e9dad01707c5e4abf7cdb11a613e0
Garry Childs <info@freedomwebservices.net>
2021-03-07 20:16:28 +0000

    Added custom error messages

--- src/Form/EmailForm.php
+++ src/Form/EmailForm.php
@@ -5,9 +5,11 @@ namespace FwsDoctrineAuth\Form;
 use Laminas\Form\Form;
 use Laminas\InputFilter\InputFilterProviderInterface;
 use Laminas\Form\Element;
+use Laminas\Validator;
 use DoctrineModule\Validator as DoctrineModuleValidator;
 use FwsDoctrineAuth\Exception\DoctrineAuthException;
 use Doctrine\ORM\EntityManager;
+use Laminas\Filter\StringToLower;
 
 /**
  * EmailForm
@@ -80,6 +82,7 @@ class EmailForm extends Form implements InputFilterProviderInterface
 
     public function getInputFilterSpecification(): array
     {
+        $filter = new StringToLower();
         return [
             $this->getIdentityName() => [
                 'required' => TRUE,
@@ -89,12 +92,24 @@ class EmailForm extends Form implements InputFilterProviderInterface
                 ],
                 'validators' => [
                     [
+                        'name' => Validator\NotEmpty::class,
+                        'break_chain_on_failure' => TRUE,
+                        'options' => [
+                            'messages' => [
+                                Validator\NotEmpty::IS_EMPTY => sprintf(_("You must specify your %s"), $filter->filter($this->config['doctrineAuth']['formElements']['identity_label'])),
+                            ],
+                        ],
+                    ],
+                    [
                         'name' => DoctrineModuleValidator\ObjectExists::class,
                         'break_chain_on_failure' => TRUE,
                         'options' => [
                             'target_class' => $this->config['doctrine']['authentication']['orm_default']['identity_class'],
                             'object_repository' => $this->entityManager->getRepository($this->config['doctrine']['authentication']['orm_default']['identity_class']),
                             'fields' => [$this->getIdentityName()],
+                            'messages' => [
+                                DoctrineModuleValidator\ObjectExists::ERROR_NO_OBJECT_FOUND => sprintf(_('This %s is not registered'), $filter->filter($this->config['doctrineAuth']['formElements']['identity_label'])),
+                            ],
                         ],
                     ],
                 ]
